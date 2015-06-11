88f510773f18a598b6f0fa47e38e648753bfc9a4
Joaquin Montoliu <jmontoliu7@gmail.com>
2015-06-11 21:20:03 +0200

    A full

--- test.py
+++ test.py
@@ -13,46 +13,53 @@ import urllib
 import json
 
 class TestsInterns(unittest.TestCase):
-    params = None
-    uid = 0
+
     
-    def testLogin(self): 
-        self.params = json.dumps({"nick":"Test", "password":"test", "master":True, "estat":"en espera", "exp":0})
+    def login(self):
+        params = json.dumps({"nick":"Test", "password":"test", "master":True, "estat":"en espera", "exp":0})
         headers = {"Content-type":"application/json"}
 	conn = httplib.HTTPConnection('localhost',80, timeout=2)
-        conn.request("POST","/api/login",self.params,headers)
+        conn.request("POST","/api/login",params,headers)
         resp = conn.getresponse()
-	data = resp.read()
+        return resp
+	
+        
+    def testLogin(self): 
+        resp = self.login()
+        data = resp.read()
         print str(data)
         self.assertEqual(resp.status, 200) # status = 200 OK
-        obj= (json.loads(data))
-        obj = json.loads(obj)
-        self.uid = (obj["uid"]) 
-        self.params = json.dumps({"id_jugador":self.uid, "avatar":"/images/heroes/cara_guerrero.jpg", "nom":"Spopobich", "live":20, "force":30, "agility":30})
+        
         
     def testcreateHeroe(self):
-        print """Nem a veure el params"""
-        print self.params
+        resp = self.login()
+        data = resp.read()
+        obj= (json.loads(data))
+        obj = json.loads(obj)
+        uid = (obj["uid"]) 
+        params = json.dumps({"id_jugador":uid, "avatar":"/images/heroes/cara_guerrero.jpg", "nom":"Spopobich", "live":20, "force":30, "agility":30})
         headers = {"Content-type":"application/json"}
         conn = httplib.HTTPConnection('localhost',80, timeout=2)
-        conn.request("POST","/api/createHeroe",self.params,headers)
+        conn.request("POST","/api/createHeroe",params,headers)
         resp = conn.getresponse()
         data = resp.read()
         print data
         self.assertEqual(resp.status, 200)
     
     def testcreateMap(self):
-        params = json.dumps({"id":self.uid,"mapName":"MapaTest","col":10,"fil":10, "background":"images/maps/bosc.jpg"})
+        resp = self.login()
+        data = resp.read()
+        obj= (json.loads(data))
+        obj = json.loads(obj)
+        uid = (obj["uid"])
+        params = json.dumps({"id":uid,"mapName":"MapaTest","col":10,"fil":10, "background":"images/maps/bosc.jpg"})
         headers = {"Content-type":"application/json"}
         conn = httplib.HTTPConnection('localhost',80, timeout=2)
         conn.request("POST","/api/createMap",params,headers)
         resp = conn.getresponse()
         data = resp.read()
         print data
-        self.assertEqual(resp.status, 200)
-        
-
-        
+        self.assertEqual(resp.status, 200) 
     
 
 if __name__ == "__main__":
